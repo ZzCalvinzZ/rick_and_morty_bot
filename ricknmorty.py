b5157ac04e5ad9eafa6894bce7e30d58ab0eb45a
Calvin Collins <calvinkcollins@gmail.com>
2016-06-02 00:25:54 -0700

    read channel messages and grab token from yaml file

--- ricknmorty.py
+++ ricknmorty.py
@@ -1,13 +1,30 @@
 import re
+import yaml
 from collections import namedtuple
 
+#get slack token from yaml file
+with open('token.yaml', 'r') as stream:
+    token = yaml.load(stream)['token']
+
+from slackclient import SlackClient
+
+#slack stuff
+sc = SlackClient(token)
+
+#bot stuff
 text = namedtuple('phrase', 'original response')
 phrases = (
     text(original=re.compile("(what is|what's) my purpose", flags=re.IGNORECASE), response="You pass butter."),
 )
 
-message = 'test, what is my purpose'
+#connect to slack and do stuff
+if sc.rtm_connect():
+    while True:
+        event = sc.rtm_read()
+        for item in event:
+            message = item.get('text', None)
 
-for phrase in phrases:
-    if phrase.original.search(message):
-        print phrase.response
+            if message:
+                for phrase in phrases:
+                    if phrase.original.search(message):
+                        print phrase.response
